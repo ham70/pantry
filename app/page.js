6a3d04778932af1588c3d80ce600039b21f7deb0
Matthew Ham <hammatthew70@gmail.com>
2024-08-02 17:12:13 -0400

    altered container height to display a minimum of the entire viewport height but can be longer incase there are many items

--- app/page.js
+++ app/page.js
@@ -1,68 +1,19 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import { Container, Typography, TextField, Grid, IconButton, Box, Button } from "@mui/material";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-
-import { collection, addDoc, getDoc, querySnapshot, query, onSnapshot, deleteDoc, doc } from "firebase/firestore";
-import {db} from './firebase'
+import { Container, Typography, Box } from "@mui/material";
 
 import AddItemsMenu from './components/addItemsMenu.js'
 import FindItemsMenu from './components/findItemsMenu.js'
 
 export default function Home() {
-  const [items, setItems] = useState([]);
-
-  const [newItem, setNewItem] = useState({name: '', quantity: ''})
-
-
-  //handling database actions====================================================================================
-  //add items to db
-  const addItem = async (e) => {
-    e.preventDefault()
-    console.log("db addition attempt");
-    setNewItem({name: '', quantity: ''});
-    
-    if(newItem.name !== '' && newItem.quantity !== ''){
-      await addDoc(collection(db, 'items'), {
-        name: newItem.name.trim(),
-        quantity: newItem.quantity,
-      });
-    }
-  }
-
-  //read items from db
-  useEffect(() => {
-    const q = query(collection(db, 'items'))
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let itemsArr = [];
-
-      querySnapshot.forEach((doc) => {
-        itemsArr.push({...doc.data(), id: doc.id})
-      })
-      setItems(itemsArr);
-    })
-  })
-
-
-  //delete items from db
-  const deleteItem = async (id) => {
-    console.log("db deletion attempt");
-  
-    try {
-      await deleteDoc(doc(db, "items", id))
-      console.log("document successfully deleted!");
-    } catch (error) {
-      console.error("Error removing document: ", error);
-    }
-  };
 
   //hmlt to be rendered on screen=============================================================================
   return (
     <Container 
     sx={{
       background: "grey",
-      height: "100vh", 
+      minHeight: "100vh", 
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
